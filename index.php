951e0c01312e6e6dc0a629469cec32041cffbbcc
Cloud Chou <515312382@qq.com>
2013-11-14 23:59:37 +0800

    从android develop弄了一点资源下来

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>mb5u.com</title>
+<style type="text/css">
+html {
+	line-height: 20px;
+}
+
+html,body {
+	background-color: #F9F9F9;
+	height: 100%;
+	margin: 0;
+	padding: 0;
+}
+
+body {
+	color: #222222;
+	font: 14px/21px Microsoft YaHei, Tahoma, Arial;
+	padding: 0 20px;
+}
+
+*::-moz-selection {
+	background-color: #0099CC;
+	color: #FFFFFF;
+}
+
+.wrap {
+	clear: both;
+	margin: 0 auto;
+	min-width: 600px;
+	width: 100%;
+}
+
+#header {
+	padding: 2.2em 0 0.2em;
+}
+
+#header,#searchResults,#nav-x {
+	margin: 0;
+}
+
+#header-wrap {
+	transition: all 0.25s ease-out 0s;
+}
+
+#header:before,#header:after {
+	clear: both;
+	content: "";
+	display: table;
+}
+
+#nav-x {
+	padding-top: 14px;
+}
+
+#body-content {
+	margin: 0;
+}
+
+.clearfix:before,.clearfix:after {
+	content: "";
+	display: table;
+}
+
+.clearfix:after {
+	clear: both;
+}
+</style>
+</head>
+<body>
+	<div id="header">
+		<div id="header-wrap" class="wrap">Android 编译系统在线参考文档</div>
+	</div>
+	<div id="searchResults" class="wrap" style="display: none;">search
+		results</div>
+	<div id="nav-x">nav-x</div>
+	<div id="body-content" class="wrap clearfix">
+		<div id="side-nav" class="col-4"
+			itemtype="http://schema.org/SiteNavigationElement" itemscope="">
+			<div id="devdoc-nav" class="" style="width: auto; left: 20px;"></div>
+			<div id="api-nav-header"></div>
+			<div id="swapper" style="height: 329px;"></div>
+			<div id="nav-swap">
+				<div id="resize-packages-nav" class="ui-resizable"
+					resize-packages-nav></div>
+				<div id="classes-nav" class="scroll-pane jspScrollable"
+					style="overflow: hidden; padding: 0px; height: 174px; margin-top: 10px; width: 372px;">
+					classes-nav</div>
+			</div>
+		</div>
+		<div id="doc-col" class="col-12">
+			test content
+		</div>
+</body>
+</html>
