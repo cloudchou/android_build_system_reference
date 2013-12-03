55d7d2fec336aca760dc628200218ead5808f607
Cloud Chou <515312382@qq.com>
2013-12-03 23:49:03 +0800

    添加统计代码和运行天数统计

--- footer.php
+++ footer.php
@@ -2,4 +2,16 @@
 	<a href="#top"> <span></span> 回到顶部
 	</a>
 </p>
-<div id="footer">Copyright©软件架构师成长之路</div>
+
+<div id="footer"> 
+	<a href="http://www.cloudchou.com">Copyright©软件架构师成长之路</a> | <span id="daycount"></span> 
+	
+	<div style="display: none">
+		<script type="text/javascript">
+			var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+			document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F50761bb5ef303074c39d254b24132503' type='text/javascript'%3E%3C/script%3E"));
+		</script>
+	</div>
+</div>
+
+
--- script/functions.js
+++ script/functions.js
@@ -1,3 +1,49 @@
+function GetDateDiff(startTime, endTime, diffType) {
+         //将xxxx-xx-xx的时间格式，转换为 xxxx/xx/xx的格式 
+         startTime = startTime.replace(/\-/g, "/");
+         endTime = endTime.replace(/\-/g, "/");
+
+         //将计算间隔类性字符转换为小写
+         diffType = diffType.toLowerCase();
+         var sTime = new Date(startTime);      //开始时间
+         var eTime = new Date(endTime);  //结束时间
+         //作为除数的数字
+         var divNum = 1;
+         switch (diffType) {
+             case "second":
+                 divNum = 1000;
+                 break;
+             case "minute":
+                 divNum = 1000 * 60;
+                 break;
+             case "hour":
+                 divNum = 1000 * 3600;
+                 break;
+             case "day":
+                 divNum = 1000 * 3600 * 24;
+                 break;
+             default:
+                 break;
+         }
+         return parseInt((eTime.getTime() - sTime.getTime()) / parseInt(divNum));
+};
+
+Date.prototype.format = function (fmt) { //author: meizz 
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+};
+
 $(document).ready(function() {
 
 	$("#s").focus(function() {
@@ -55,5 +101,12 @@ $(document).ready(function() {
 			return false;
 		});
 	});
-
+	
+   //网站计时	
+    
+   var date=new Date(); 
+   var dateStr=date.format("yyyy-MM-dd");
+   var daycount=GetDateDiff("2013-12-1",dateStr, "day");
+   var str="本网站开通于2013.12.1，已运行"+daycount+"天";
+   $("#daycount").text(str);
 });
