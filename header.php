d98f28938c4474796d20f95ae4886ae89f4d84f6
Cloud Chou <515312382@qq.com>
2013-12-04 22:00:50 +0800

    作者博客链接在新窗口打开

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
 <div id="header">
-	<h1><a href="/">Android编译系统参考手册</a></h1> <h3><a href="http://www.cloudchou.com">作者博客: http://www.cloudchou.com</a></h3>
+	<h1><a href="/">Android编译系统参考手册</a></h1> <h3><a href="http://www.cloudchou.com" target="_blank">作者博客: http://www.cloudchou.com</a></h3>
 </div>
 
