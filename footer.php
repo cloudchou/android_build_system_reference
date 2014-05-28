386e03fa94ba9b1ea4654e3b47721c3e2c2062d0
cloud <515312382@qq.com>
2014-05-28 09:41:35 +0800

    添加cnzz统计

--- footer.php
+++ footer.php
@@ -11,6 +11,10 @@
 			var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
 			document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F50761bb5ef303074c39d254b24132503' type='text/javascript'%3E%3C/script%3E"));
 		</script>
+        <script type="text/javascript">
+            var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+            document.write(unescape("%3Cspan id='cnzz_stat_icon_1000385810'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "v1.cnzz.com/z_stat.php%3Fid%3D1000385810' type='text/javascript'%3E%3C/script%3E"));
+        </script>
 	</div>
 </div>
 
